457dbf69f9965473a965a1b5f011d69a17548dd7
PeterBrr <dinhquangnhat16062006@gmail.com>
2025-11-22 21:16:01 +0700

    commit Bai06

--- Bai06.Designer.cs
+++ Bai06.Designer.cs
@@ -29,11 +29,192 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSource = new System.Windows.Forms.TextBox();
+            this.txtDest = new System.Windows.Forms.TextBox();
+            this.btnSelectSource = new System.Windows.Forms.Button();
+            this.btnSelectDest = new System.Windows.Forms.Button();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lblStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnCopy);
+            this.groupBox1.Controls.Add(this.btnSelectDest);
+            this.groupBox1.Controls.Add(this.btnSelectSource);
+            this.groupBox1.Controls.Add(this.txtDest);
+            this.groupBox1.Controls.Add(this.txtSource);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.ForeColor = System.Drawing.Color.Blue;
+            this.groupBox1.Location = new System.Drawing.Point(43, 35);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(673, 159);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Sao chép tập tin";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(35, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(171, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Đường Dẫn Thư Mục Nguồn";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(35, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(157, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Đường Dẫn Thư Mục Đích";
+            // 
+            // txtSource
+            // 
+            this.txtSource.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtSource.Location = new System.Drawing.Point(235, 27);
+            this.txtSource.Name = "txtSource";
+            this.txtSource.Size = new System.Drawing.Size(380, 22);
+            this.txtSource.TabIndex = 2;
+            // 
+            // txtDest
+            // 
+            this.txtDest.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtDest.Location = new System.Drawing.Point(235, 75);
+            this.txtDest.Name = "txtDest";
+            this.txtDest.Size = new System.Drawing.Size(380, 22);
+            this.txtDest.TabIndex = 3;
+            // 
+            // btnSelectSource
+            // 
+            this.btnSelectSource.AutoSize = true;
+            this.btnSelectSource.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btnSelectSource.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSelectSource.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSelectSource.ForeColor = System.Drawing.Color.Blue;
+            this.btnSelectSource.Location = new System.Drawing.Point(628, 25);
+            this.btnSelectSource.Name = "btnSelectSource";
+            this.btnSelectSource.Size = new System.Drawing.Size(39, 28);
+            this.btnSelectSource.TabIndex = 4;
+            this.btnSelectSource.Text = "...";
+            this.btnSelectSource.UseVisualStyleBackColor = false;
+            this.btnSelectSource.Click += new System.EventHandler(this.btnSelectSource_Click);
+            // 
+            // btnSelectDest
+            // 
+            this.btnSelectDest.AutoSize = true;
+            this.btnSelectDest.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btnSelectDest.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSelectDest.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSelectDest.ForeColor = System.Drawing.Color.Blue;
+            this.btnSelectDest.Location = new System.Drawing.Point(628, 72);
+            this.btnSelectDest.Name = "btnSelectDest";
+            this.btnSelectDest.Size = new System.Drawing.Size(39, 28);
+            this.btnSelectDest.TabIndex = 5;
+            this.btnSelectDest.Text = "...";
+            this.btnSelectDest.UseVisualStyleBackColor = false;
+            this.btnSelectDest.Click += new System.EventHandler(this.btnSelectDest_Click);
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btnCopy.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCopy.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnCopy.Location = new System.Drawing.Point(235, 122);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(182, 30);
+            this.btnCopy.TabIndex = 6;
+            this.btnCopy.Text = "Sao Chép";
+            this.btnCopy.UseVisualStyleBackColor = false;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.progressBar1);
+            this.groupBox2.ForeColor = System.Drawing.Color.Blue;
+            this.groupBox2.Location = new System.Drawing.Point(43, 218);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(673, 68);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Tiến trình sao chép";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.progressBar1.Location = new System.Drawing.Point(38, 30);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(629, 23);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblStatus});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 292);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(728, 26);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(115, 20);
+            this.lblStatus.Text = "Đang Sao Chép:";
+            // 
+            // Bai06
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(728, 318);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Bai06";
             this.Text = "Bai06";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.Button btnSelectDest;
+        private System.Windows.Forms.Button btnSelectSource;
+        private System.Windows.Forms.TextBox txtDest;
+        private System.Windows.Forms.TextBox txtSource;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lblStatus;
     }
 }
--- Bai06.cs
+++ Bai06.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,108 @@ namespace BTH4
         public Bai06()
         {
             InitializeComponent();
+        }
+
+        private void btnSelectSource_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.Description = "Chọn thư mục nguồn chứa file";
+
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                txtSource.Text = fbd.SelectedPath;
+            }
+        }
+
+        private void btnSelectDest_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.Description = "Chọn thư mục đích để lưu file";
+
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                txtDest.Text = fbd.SelectedPath;
+            }
+        }
+
+        private async void btnCopy_Click(object sender, EventArgs e)
+        {
+            string sourcePath = txtSource.Text;
+            string destPath = txtDest.Text;
+
+            // Kiểm tra đầu vào
+            if (string.IsNullOrEmpty(sourcePath) || string.IsNullOrEmpty(destPath))
+            {
+                MessageBox.Show("Vui lòng chọn đầy đủ thư mục nguồn và đích!");
+                return;
+            }
+
+            if (!Directory.Exists(sourcePath))
+            {
+                MessageBox.Show("Thư mục nguồn không tồn tại!");
+                return;
+            }
+
+            // Tạo thư mục đích nếu chưa có
+            if (!Directory.Exists(destPath))
+            {
+                Directory.CreateDirectory(destPath);
+            }
+
+            // Lấy danh sách tất cả các file trong thư mục nguồn
+            string[] files = Directory.GetFiles(sourcePath);
+            int totalFiles = files.Length;
+
+            if (totalFiles == 0)
+            {
+                MessageBox.Show("Thư mục nguồn trống!");
+                return;
+            }
+
+            // Cấu hình ProgressBar
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = totalFiles;
+            progressBar1.Value = 0;
+
+            // Khóa nút Copy để tránh bấm nhiều lần
+            btnCopy.Enabled = false;
+
+            await Task.Run(() =>
+            {
+                for (int i = 0; i < totalFiles; i++)
+                {
+                    string sourceFile = files[i];
+                    string fileName = Path.GetFileName(sourceFile);
+                    string destFile = Path.Combine(destPath, fileName);
+
+                    // Thực hiện copy (overwrite = true: ghi đè nếu trùng)
+                    File.Copy(sourceFile, destFile, true);
+
+                    // Cập nhật giao diện (Phải dùng Invoke vì đang ở luồng phụ)
+                    this.Invoke(new Action(() =>
+                    {
+                        // Cập nhật thanh tiến trình
+                        progressBar1.Value = i + 1;
+
+                        // Cập nhật Label trạng thái bên dưới
+                        string statusMsg = $"Đang sao chép: {fileName}";
+                        lblStatus.Text = statusMsg;
+
+                        // Cập nhật ToolTip cho ProgressBar
+                        // Khi chuột hover vào ProgressBar sẽ thấy tên file đang chạy
+                        toolTip1.SetToolTip(progressBar1, statusMsg);
+                    }));
+
+                    // Giả lập độ trễ 1 chút để mắt thường kịp nhìn thấy tiến trình chạy                    
+                    System.Threading.Thread.Sleep(100);
+                }
+            });
+
+            // Mở khóa nút và thông báo sau khi xong
+            btnCopy.Enabled = true;
+            lblStatus.Text = "Sao chép hoàn tất!";
+            MessageBox.Show("Đã sao chép thành công tất cả tập tin!");
+
         }
     }
 }
