390841140f265560faf71cbe807e8da663a457d1
PeterBrr <dinhquangnhat16062006@gmail.com>
2025-11-22 11:37:15 +0700

    commit Bai01

---
+++ Bai01.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace BTH4
+{
+    partial class Bai01
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblInfo.Location = new System.Drawing.Point(0, 0);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(565, 374);
+            this.lblInfo.TabIndex = 0;
+            this.lblInfo.Text = "label1";
+            this.lblInfo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblInfo.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            // 
+            // Bai01
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(565, 374);
+            this.Controls.Add(this.lblInfo);
+            this.Name = "Bai01";
+            this.Text = "Bai01";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblInfo;
+    }
+}
+
---
+++ Bai01.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace BTH4
+{
+    public partial class Bai01 : Form
+    {       
+        public Bai01()
+        {
+            InitializeComponent();
+            // Kích hoạt chế độ KeyPreview để Form nhận sự kiện bàn phím trước các điều khiển con
+            this.KeyPreview = true;
+        }
+
+        // Xử lý sự kiện click chuột
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            int x = e.X;
+            int y = e.Y;
+
+            // Nếu click vào Label (sender là lblInfo), cộng thêm vị trí của Label
+            if (sender == lblInfo)
+            {
+                x += lblInfo.Location.X;
+                y += lblInfo.Location.Y;
+            }
+
+            // Cập nhật text
+            lblInfo.Text = $"CHUỘT CLICK\nNút: {e.Button}\nTọa độ trên Form: X={x}, Y={y}";
+        }
+
+        // Xử lý sự kiện nhấn phím (Dùng để lấy KeyCode, Modifiers như Ctrl, Shift)
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            string info = $"Phím (KeyDown): KeyCode={e.KeyCode}, KeyValue={e.KeyValue}, Modifiers={e.Modifiers}";
+            lblInfo.Text = info;
+        }
+
+        // Xử lý sự kiện gõ phím (Dùng để lấy mã ASCII / ký tự thực tế)
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Lấy mã ASCII
+            int asciiCode = (int)e.KeyChar;
+
+            string info = $"Ký tự (KeyPress): '{e.KeyChar}' | Mã ASCII: {asciiCode}";
+
+            // Nối tiếp thông tin vào Label (vì KeyDown chạy trước KeyPress)
+            lblInfo.Text += $"\n{info}";
+        }
+    }
+}
